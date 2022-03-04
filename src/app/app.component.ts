1f7ab02714e3e2907edc48a260c736a7d177e10f
unknown <Aziz13.id@gmail.com>
2022-03-04 19:39:56 +0700

    Angular Three.js BAsic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core'; 
+import { ThreeService } from './services/three.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Pano';
+  @ViewChild('rendererCanvas', { static: true })
+  public rendererCanvas!: ElementRef<HTMLCanvasElement>;
+
+  public constructor(private threeService: ThreeService){
+    
+  }
+
+  ngOnInit(): void {
+    this.threeService.createScene(this.rendererCanvas);
+    this.threeService.animate();
+  }
 }
